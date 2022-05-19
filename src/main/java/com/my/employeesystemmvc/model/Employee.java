3383d4ef50a6679167ec00e44f69c396f8f49181
Yashar Mustafayev <yasarmustafayev1998@gmail.com>
2022-05-19 23:36:48 +0200

    entity class was created

---
+++ src/main/java/com/my/employeesystemmvc/model/Employee.java
@@ -0,0 +1,26 @@
+package com.my.employeesystemmvc.model;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Employee {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Integer id;
+
+    String firstName;
+
+    String lastName;
+
+    String email;
+}
