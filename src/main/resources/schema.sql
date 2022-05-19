7880e669dddb43db9645b1b017ce6259591d844a
Yashar Mustafayev <yasarmustafayev1998@gmail.com>
2022-05-19 23:33:14 +0200

    executable script on startup was added to resources

---
+++ src/main/resources/schema.sql
@@ -0,0 +1,7 @@
+CREATE TABLE employee(
+    id int not null auto_increment,
+    first_name varchar(45) default null,
+    last_name varchar(45) default null,
+    email varchar(45) default null,
+    primary key(id)
+);
