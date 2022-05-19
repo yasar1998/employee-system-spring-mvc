86c5e07293a0571680ac7194fab8ad2d2a69b2bb
Yashar Mustafayev <yasarmustafayev1998@gmail.com>
2022-05-19 23:09:27 +0200

    initial import

---
+++ src/main/java/com/my/employeesystemmvc/EmployeeSystemMvcApplication.java
@@ -0,0 +1,13 @@
+package com.my.employeesystemmvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeSystemMvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeSystemMvcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/my/employeesystemmvc/EmployeeSystemMvcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.my.employeesystemmvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeSystemMvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
